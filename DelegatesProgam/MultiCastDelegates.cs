a008c3b2b5bc009c777b1029dbb94d78d789a0bb
saifalishaikh31 <rockyasaifalishaikh@gmail.com>
2022-02-16 14:45:39 +0530

    [Saif Ali] Add : Multi Cast Delegates Added

---
+++ DelegatesProgam/MultiCastDelegates.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesProgam
+{
+    public delegate void DelOp(int x, int y);
+    internal class MultiCastDelegates
+    {
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine("Addition of two numbers       : {0}", a + b);
+        }
+        public static void Sub(int a, int b)
+        {
+            Console.WriteLine("Substraction of two numbers   : {0}", a - b);
+        }
+        public static void Mul(int a, int b)
+        {
+            Console.WriteLine("Multiplication of two numbers : {0}", a * b);
+        }
+        public static void Div(int a, int b)
+        {
+            Console.WriteLine("Division of two numbers       : {0}", a / b);
+        }
+    }
+}
--- DelegatesProgam/Program.cs
+++ DelegatesProgam/Program.cs
@@ -10,9 +10,37 @@ namespace DelegatesProgam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Single Cast Delegates");
-            operation obj = SingleCastDelegates.Add;
-            obj(5, 5);
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\n 1.Single Cast Delegates."
+                                + "\n 2.Multi Cast Delegates."
+                                + "\n 3.Exit\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Single Cast Delegates");
+                        operation obj = SingleCastDelegates.Add;
+                        obj(5, 5);
+                        break;
+                    case 2:
+                        Console.WriteLine("Multi Cast Delegates");
+                        DelOp delOp = MultiCastDelegates.Add;
+                        delOp += MultiCastDelegates.Sub;
+                        delOp += MultiCastDelegates.Mul;
+                        delOp += MultiCastDelegates.Div;
+                        delOp(5, 5);
+                        delOp(8, 3);
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter correct option!!!");
+                        break;
+                }
+            }
             Console.ReadLine();
         }
     }
