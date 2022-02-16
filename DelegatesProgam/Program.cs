dec17a465eb7a1f40a1f0b2626fb494e0c0447de
saifalishaikh31 <rockyasaifalishaikh@gmail.com>
2022-02-16 14:23:16 +0530

    [Saif Ali] Add : Single Cast Delegates Added

---
+++ DelegatesProgam/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesProgam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Single Cast Delegates");
+            operation obj = SingleCastDelegates.Add;
+            obj(5, 5);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DelegatesProgam/SingleCastDelegates.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesProgam
+{
+    public delegate void operation(int x, int y);
+    public class SingleCastDelegates
+    {
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine("Addition of two numbers : {0}", a+b);
+        }
+    }
+}
